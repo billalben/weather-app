ae7e64623f0a9f83a176ebf08f219e50d31d08e1
Benzazoua <billalben252@gmail.com>
2024-03-13 20:30:38 +0100

    JavaScript: All fetch url configuration

---
+++ assets/js/api.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const api_key = "3dccee85156ffdd15e8ce63e0e1512a2";
+
+/**
+ * Fetch Data from server
+ * @param {string} URL - API URL
+ * @param {Function} callback - Callback function
+ */
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}`;
+  },
+  reverseGeocoding(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5`;
+  },
+  /**
+   * @param {string} query - City name e.g. London, New York, Tlemcen ...
+   * @returns
+   */
+  geocoding(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
