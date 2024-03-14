d995acd30799214be82680e302c77d9a08816aac
Benzazoua <billalben252@gmail.com>
2024-03-14 15:47:57 +0100

    JavaScript: Search API integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,3 +22,67 @@ const searchTogglers = document.querySelectorAll("[data-search-toggler]");
 
 const togglerSearch = () => searchView.classList.toggle("active");
 addEventOnElements(searchTogglers, "click", togglerSearch);
+
+// SEARCH INTERACTION
+const searchField = document.querySelector("[data-search-field]");
+const searchResult = document.querySelector("[data-search-result]");
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener("input", function () {
+  searchTimeout ?? clearTimeout(searchTimeout);
+
+  if (!searchField.value) {
+    searchResult.classList.remove("active");
+    searchResult.innerHTML = "";
+    searchField.classList.remove("searching");
+  } else {
+    searchField.classList.add("searching");
+  }
+
+  if (searchField.value) {
+    searchTimeout = setTimeout(() => {
+      fetchData(url.geocoding(searchField.value), function (locations) {
+        searchField.classList.remove("searching");
+        searchResult.classList.add("active");
+        searchResult.innerHTML = `
+          <ul class="view-list" data-search-list></ul>
+        `;
+
+        const /** {NodeList} | [] */ items = [];
+
+        for (const {
+          name,
+          lat: latitude,
+          lon: longitude,
+          country,
+          state,
+        } of locations) {
+          const searchItem = document.createElement("li");
+          searchItem.classList.add("view-item");
+
+          searchItem.innerHTML = `
+            <span class="m-icon">location_on</span>
+            <div>
+              <p class="item-title">${name}</p>
+              <p class="label-2 item-subtitle">${state || ""} ${country}</p>
+            </div>
+            <a
+              href="#/weather?lat=${latitude}&lon=${longitude}"
+              class="item-link has-state"
+              aria-label="${name} weather"
+              data-search-toggler
+            ></a>
+          `;
+
+          searchResult
+            .querySelector("[data-search-list]")
+            .appendChild(searchItem);
+
+          items.push(searchItem.querySelector("[data-search-toggler]"));
+        }
+      });
+    }, searchTimeoutDuration);
+  }
+});
